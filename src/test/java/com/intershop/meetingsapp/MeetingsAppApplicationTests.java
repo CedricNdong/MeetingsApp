9eb581b5e017d1c9b7279d2608a0121095f851f5
CedricNdong <juniorndong901@gmail.com>
2023-02-10 17:40:05 +0100

    project initialization

---
+++ src/test/java/com/intershop/meetingsapp/MeetingsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intershop.meetingsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
